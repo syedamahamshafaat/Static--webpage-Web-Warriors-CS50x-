2d9c9b680261dd2acea3d52d89b2e6e1124dd831
syedamahamshafaat <96385759+syedamahamshafaat@users.noreply.github.com>
2025-09-06 16:58:51 +0100

    Delete Templates/script.js

--- Templates/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-    alert("Hi! Welcome to my first ever static web page!")
-})
-
-const sloganElement = document.getElementById('slogan');
-            sloganElement.textContent = "Explore, Create and Conquer with Web Warriors!";
-
