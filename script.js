824f1eb707b1c8526521fd9b420bbef12928fe9c
syedamahamshafaat <96385759+syedamahamshafaat@users.noreply.github.com>
2025-09-06 16:47:35 +0100

    Add main website files and remove templates folder

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    alert("Hi! Welcome to my first ever static web page!")
+})
+
+const sloganElement = document.getElementById('slogan');
+            sloganElement.textContent = "Explore, Create and Conquer with Web Warriors!";
+
